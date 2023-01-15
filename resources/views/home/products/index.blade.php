b2da75022550355c786101d6317ac6f54582dbba
ghelgameshra <ghelgameshra3347@gmail.com>
2023-01-16 05:12:13 +0700

    membenahi halaman index products

--- resources/views/home/products/index.blade.php
+++ resources/views/home/products/index.blade.php
@@ -9,27 +9,28 @@
     <div class="row justify-content-center">
 
         @foreach ($products as $product)    
-        <div class="col-lg-3 rounded mt-3">
-            <div class="card text-black">
-            <img src="{{ $product->image }}" class="card-img-top img-fluid border-0 m-auto mt-3 rounded" alt="{{ $product->name }}" style="height: auto; width: 60%">
-            <div class="card-body">
+        <div class="col-md-2 rounded mt-3">
+            <div class="card text-black h-100 shadow">
+            <img src="{{ $product->image }}" class="card-img-top img-fluid border-0 m-auto mt-3 rounded" alt="{{ $product->name }}" style="height: auto; width: 50%">
+            <div class="card-body d-flex flex-column">
                 <div class="text-center">
-                <h5 class="card-title">{{ $product->name }}</h5>
-                <p class="text-muted mb-4">{{ $product->category->name }}</p>
-                </div>
-                <div>
-                <div class="d-flex justify-content-between">
-                    <span>Price</span><span>@currency($product->price)</span>
-                </div>
-                <div class="d-flex justify-content-center mt-2">
-                    <a href="/home/products/{{ $product->product_id }}" class="nav-link badge p-2 m-1" style="background-color: #183153"><i class="fa-solid fa-eye"></i> Details</a>
-                    <a href="/home/products/{{ $product->product_id }}/edit" class="nav-link badge p-2 m-1 bg-warning"><i class="fa-solid fa-pencil"></i> edit</a>
-                    <form action="/home/products/{{ $product->product_id }}" method="post" class="d-inline">
-                        @method('delete')
-                        @csrf
-                        <button class="badge bg-danger border-0 p-2 m-1" onclick="return confirm('Are you sure ?')"><i class="fa-solid fa-ban"></i> Delete</button>
-                    </form>
-                </div>
+                <h5 class="card-title" style="font-size: 15px; font-weight: 600">{{ $product->name }}</h5>
+            </div>
+
+                <div class="mt-auto" style="margin-bottom: -25px">
+                    <p class="text-muted mb-3 text-center">{{ $product->category->name }}</p>
+                    <div class="d-flex justify-content-between" style="font-size: 14px">
+                        <span>Price</span><span>@currency($product->price)</span>
+                    </div>
+                    <div class="d-flex justify-content-center mt-2" style="font-size: 12px">
+                        <a href="/home/products/{{ $product->product_id }}" class="nav-link badge p-2 m-1" style="background-color: #183153"><i class="fa-solid fa-eye"></i> Details</a>
+                        <a href="/home/products/{{ $product->product_id }}/edit" class="nav-link badge p-2 m-1 bg-warning"><i class="fa-solid fa-pencil"></i> edit</a>
+                        <form action="/home/products/{{ $product->product_id }}" method="post" class="d-inline">
+                            @method('delete')
+                            @csrf
+                            <button class="nav-link badge bg-danger border-0 p-2 m-1" onclick="return confirm('Are you sure ?')"><i class="fa-solid fa-ban"></i> Delete</button>
+                        </form>
+                    </div>
                 </div>
             </div>
             </div>
