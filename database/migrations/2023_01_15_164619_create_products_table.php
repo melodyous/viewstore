62f149090bcf6b74667326b073810d3db867ff0d
ghelgameshra <ghelgameshra3347@gmail.com>
2023-01-16 00:16:24 +0700

    membuat fungsi menampilkan nilai rupiah

--- database/migrations/2023_01_15_164619_create_products_table.php
+++ database/migrations/2023_01_15_164619_create_products_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->string('name');
             $table->integer('price');
             $table->integer('stock');
+            $table->string('image');
             $table->timestamps();
         });
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Product;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 
@@ -15,6 +17,35 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+
+        // $table->id();
+        // $table->foreignId('category_id');
+        // $table->string('product_id');
+        // $table->string('name');
+        // $table->integer('price');
+        // $table->integer('stock');
+        // $table->timestamps();
+
+        Product::create([
+            'category_id' => 1,
+            'product_id' => 'amd-a8-7680-apu-fm2+',
+            'name' => 'AMD A8-7680 APU - FM2+',
+            'price' => 610000,
+            'stock' => 100,
+            'image' => '/storage/product-images/a8.jpg'
+        ]);
+
+        Product::create([
+            'category_id' => 1,
+            'product_id' => 'amd-ryzen-3-3200g-am4',
+            'name' => 'AMD Ryzen 3 3200G AM4',
+            'price' => 1390000,
+            'stock' => 50,
+            'image' => '/storage/product-images/ryzen3.jpg'
+        ]);
+
+
+
         User::create([
             'name' => 'Siska Rahmawati',
             'username' => 'sisk123',
